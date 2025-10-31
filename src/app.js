6c433a84e35c72018d39aec7700b2689aeaff96b
vasavipalaka <palakavasavi0892@gmail.com>
2025-10-31 18:25:12 +0530

    post,put,delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req,res)=>{
-    res.send("Hello Vasavi!! ")
-})//request handler
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Pawan",lastName:"Vaibhav"});
+});
+
+app.post("/user",(req,res)=>{
+    //saving data to DB
+    res.send("Data successfully saved to database!");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully!!");
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("patched successfully!!");
+})
+app.put("/user",(req,res)=>{
+    res.send("kept successfully!!");
+})
 
 app.use("/test",(req,res)=>{
     res.send("i can test now!")
-})
+});
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello Pawan vaibhav!!!")
-})
 
 app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777..")
